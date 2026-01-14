d0161afe7b9a90abee82cceca6743450aee8b5db
DESKTOP-L7EVRSK\rjtim <RjTima@gmail.com>
2026-01-14 23:42:37 +0300

    add widget pause

--- Source/ProjectNova/Public/PNCoreTypes.h
+++ Source/ProjectNova/Public/PNCoreTypes.h
@@ -110,3 +110,31 @@ struct FGameData
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Game", meta = (ClampMin = "3", ClampMax = "20"))
 	int32 RespawnTime = 5; // seconds
 };
+
+UENUM(BlueprintType)
+enum class EPNMatchState : uint8
+{
+	WaitingToStart = 0,
+	InProgress,
+	Pause,
+	GameOver
+};
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnMatchStateChangedSignature, EPNMatchState);
+
+USTRUCT(BlueprintType)
+struct FLevelData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game")
+	FName LevelName = NAME_None;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game")
+	FName LevelDisplayName = NAME_None;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game")
+	UTexture2D* LevelThumb;
+};
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnLevelSelectedSignature, const FLevelData&);
